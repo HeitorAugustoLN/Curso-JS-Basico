630275550b93080c1dfe8088e01ef69793df65e7
Heitor Augusto <heitoraugustoln@outlook.com>
2020-04-26 22:06:43 -0300

    📦 NEW: Adicionar o Exercício 09 do Módulo D

---
+++ Módulos/Módulo D - Condições em JavaScript/Aulas/Aula 11/Exercício 09.js
@@ -0,0 +1,8 @@
+var país = "Brasil" // Escolhe o país
+
+console.log(`Vivendo em ${país}`) // Mostra em que país você está vivendo
+if (país == "Brasil") { // Se você mora no Brasil
+    console.log("Você é brasileiro") // Mostra que você é brasileiro
+} else { // Caso contrário
+    console.log("Você é estrangeiro") // Mostra que você é estrangeiro
+}
