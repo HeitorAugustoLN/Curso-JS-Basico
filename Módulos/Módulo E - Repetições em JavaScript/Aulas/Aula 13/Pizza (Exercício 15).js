2a7a48a97dea2b9113bf70f968552ac65110462b
Heitor Augusto <heitoraugustoln@outlook.com>
2020-04-26 22:40:24 -0300

    📦 NEW: Adicionar o Exercício 15 do Módulo E

---
+++ Módulos/Módulo E - Repetições em JavaScript/Aulas/Aula 13/Pizza (Exercício 15).js
@@ -0,0 +1,17 @@
+// Estrutura de repetição com teste lógico no final
+
+var fatia = 1;
+do {
+	console.log(`Comendo fatia ${fatia}`);
+	fatia++;
+} while (fatia <= 8);
+
+/* Estrutura de repetição com teste lógico no inicio
+
+var fatia = 1;
+
+while (fatia <= 8) {
+	console.log(`Comendo fatia ${fatia}`);
+	fatia++;
+}
+*/
